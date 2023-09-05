c37804d60b1a8bed159e0ffa5ec81134e8304eb8
barbaraLuersen <barbaraluersenlima@gmail.com>
2023-09-04 21:02:52 -0300

    FILTROS

--- src/main/java/br/sc/senac/dw/exemplos/dw/model/seletor/ProdutoSeletor.java
+++ src/main/java/br/sc/senac/dw/exemplos/dw/model/seletor/ProdutoSeletor.java
@@ -1,5 +1,93 @@
 package br.sc.senac.dw.exemplos.dw.model.seletor;
 
+import java.time.LocalDate;
+
 public abstract class ProdutoSeletor extends BaseSeletor {
 
+	private String nome;
+	private String fabricante;
+	private Double valorMinimo;
+	private Double valorMaximo;
+	private Double pesoMinimo;
+	private Double pesoMaximo;
+	private LocalDate dataCadastroInicial;
+	private LocalDate dataCadastroFinal;
+
+	public ProdutoSeletor(int pagina, int limite, String nome, String fabricante, Double valorMinimo,
+			Double valorMaximo, Double pesoMinimo, Double pesoMaximo, LocalDate dataCadastroInicial,
+			LocalDate dataCadastroFinal) {
+		super(pagina, limite);
+		this.nome = nome;
+		this.fabricante = fabricante;
+		this.valorMinimo = valorMinimo;
+		this.valorMaximo = valorMaximo;
+		this.pesoMinimo = pesoMinimo;
+		this.pesoMaximo = pesoMaximo;
+		this.dataCadastroInicial = dataCadastroInicial;
+		this.dataCadastroFinal = dataCadastroFinal;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public Double getValorMinimo() {
+		return valorMinimo;
+	}
+
+	public void setValorMinimo(Double valorMinimo) {
+		this.valorMinimo = valorMinimo;
+	}
+
+	public Double getValorMaximo() {
+		return valorMaximo;
+	}
+
+	public void setValorMaximo(Double valorMaximo) {
+		this.valorMaximo = valorMaximo;
+	}
+
+	public Double getPesoMinimo() {
+		return pesoMinimo;
+	}
+
+	public void setPesoMinimo(Double pesoMinimo) {
+		this.pesoMinimo = pesoMinimo;
+	}
+
+	public Double getPesoMaximo() {
+		return pesoMaximo;
+	}
+
+	public void setPesoMaximo(Double pesoMaximo) {
+		this.pesoMaximo = pesoMaximo;
+	}
+
+	public LocalDate getDataCadastroInicial() {
+		return dataCadastroInicial;
+	}
+
+	public void setDataCadastroInicial(LocalDate dataCadastroInicial) {
+		this.dataCadastroInicial = dataCadastroInicial;
+	}
+
+	public LocalDate getDataCadastroFinal() {
+		return dataCadastroFinal;
+	}
+
+	public void setDataCadastroFinal(LocalDate dataCadastroFinal) {
+		this.dataCadastroFinal = dataCadastroFinal;
+	}
 }
---
+++ src/main/java/br/sc/senac/dw/exemplos/dw/model/specification/ProdutoSpecification.java
@@ -0,0 +1,96 @@
+package br.sc.senac.dw.exemplos.dw.model.specification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import br.sc.senac.dw.exemplos.dw.model.entidade.Produto;
+import br.sc.senac.dw.exemplos.dw.model.seletor.ProdutoSeletor;
+import jakarta.persistence.criteria.Predicate;
+
+public class ProdutoSpecification {
+
+	/*
+	 * root: é uma referência à entidade raiz que você está consultando. No contexto
+	 * do JPA, isso representa a tabela do banco de dados correspondente à entidade
+	 * Produto.
+	 * 
+	 * query: O parâmetro query representa a consulta JPA que está sendo construída.
+	 * Ele é usado para adicionar cláusulas WHERE, JOIN, ORDER BY, entre outras, à
+	 * consulta.
+	 * 
+	 * cb (CriteriaBuilder): é uma interface que oferece métodos para construção de
+	 * cláusulas de consulta de forma programática. Você usa métodos deste objeto
+	 * para criar expressões de predicado, funções agregadas e outras partes da
+	 * consulta.
+	 * 
+	 */
+
+	public static Specification<Produto> comFiltros(ProdutoSeletor seletor) {
+		return (root, query, cb) -> {
+			List<Predicate> predicates = new ArrayList<>();
+
+			// COMO FILTRAR "PRODUTO.NOME"
+			if (seletor.getNome() != null) {
+				// WHERE/AND COLUNA OPERADOR VALOR
+				// WHERE nome like %Café%
+				predicates.add(cb.like(cb.lower(root.get("nome")), "%" + seletor.getNome().toLowerCase() + "%"));
+			}
+
+			// COMO FILTRAR POR "FABRICANTES.NOME"
+			if (seletor.getFabricante() != null) {
+				// WHERE p.fabricante like '%Rider%'
+				// WHERE f.nome like '%Rider%'
+				// JPQL = Java Persistence Query Language
+				predicates.add(
+						cb.like(root.join("fabricanteDoProduto").get("nome"), "%" + seletor.getFabricante() + "%"));
+			}
+
+			// COMO FILTRAR POR "PRODUTO.PESO"
+			if (seletor.getPesoMinimo() != null && seletor.getPesoMaximo() != null) {
+				// WHERE peso BETWEEN min AND max
+				predicates.add(cb.between(root.get("peso"), seletor.getPesoMinimo(), seletor.getPesoMaximo()));
+			} else if (seletor.getPesoMinimo() != null) {
+				// WHERE peso >= min
+				predicates.add(cb.greaterThanOrEqualTo(root.get("peso"), seletor.getPesoMinimo()));
+			} else if (seletor.getPesoMaximo() != null) {
+				// WHERE peso <= max
+				predicates.add(cb.lessThanOrEqualTo(root.get("peso"), seletor.getPesoMaximo()));
+			}
+
+			// COMO FILTRAR POR "PRODUTO.VALOR"
+			if (seletor.getPesoMinimo() != null && seletor.getPesoMaximo() != null) {
+				// WHERE valor BETWEEN min AND max
+				predicates.add(cb.between(root.get("valor"), seletor.getValorMinimo(), seletor.getValorMaximo()));
+			} else if (seletor.getValorMinimo() != null) {
+				// WHERE valor >= min
+				predicates.add(cb.greaterThanOrEqualTo(root.get("valor"), seletor.getValorMinimo()));
+			} else if (seletor.getValorMinimo() != null) {
+				// WHERE valor <= max
+				predicates.add(cb.lessThanOrEqualTo(root.get("valor"), seletor.getValorMaximo()));
+			}
+
+			// COMO FILTRAR POR "PRODUTO.DATA"
+			if (seletor.getDataCadastroInicial() != null & seletor.getDataCadastroFinal() != null) {
+				// WHERE data BETWEEN min AND max
+				predicates.add(
+						cb.between(root.get("data"), seletor.getDataCadastroInicial(), seletor.getDataCadastroFinal()));
+			} else if (seletor.getDataCadastroInicial() != null) {
+				// WHERE data >= min
+				predicates.add(cb.greaterThanOrEqualTo(root.get("data"), seletor.getDataCadastroInicial()));
+			} else if (seletor.getDataCadastroFinal() != null) {
+				// WHERE data <= max
+				predicates.add(cb.lessThanOrEqualTo(root.get("data"), seletor.getDataCadastroFinal()));
+			}
+
+			// COMO FILTRAR POR "FABRICANTE.CNPJ"
+			if (seletor.getFabricante() != null) {
+				predicates.add(
+						cb.like(root.join("fabricanteDoProduto").get("cnpj"), "%" + seletor.getFabricante() + "%"));
+			}
+
+			return cb.and(predicates.toArray(new Predicate[0]));
+		};
+	}
+}
