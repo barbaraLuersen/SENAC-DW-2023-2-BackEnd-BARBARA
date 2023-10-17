30117ec496fc380f232afea8d45c28f81540cb17
Bárbara Luersen <barbaraluersenlima@gmail.com>
2023-10-17 11:13:47 -0300

    !!!Criar método listarTodos() na classe FabricanteController.

--- src/main/java/br/sc/senac/dw/exemplos/dw/controller/FabricanteController.java
+++ src/main/java/br/sc/senac/dw/exemplos/dw/controller/FabricanteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.sc.senac.dw.exemplos.dw.exception.CampoInvalidoException;
 import br.sc.senac.dw.exemplos.dw.model.entidade.Fabricante;
+import br.sc.senac.dw.exemplos.dw.model.entidade.Produto;
 import br.sc.senac.dw.exemplos.dw.model.seletor.FabricanteSeletor;
 import br.sc.senac.dw.exemplos.dw.service.FabricanteService;
 
@@ -30,6 +32,13 @@ public class FabricanteController {
 	@PostMapping("/filtro")
 	public List<Fabricante> listarComSeletor(@RequestBody FabricanteSeletor seletor) {
 		return fabricanteService.listarComSeletor(seletor);
+	}
+	
+	@GetMapping(path = "/todos")
+	public List<Fabricante> listarTodos() {
+		//TODO
+		//return fabricanteService.listarTodos();
+		return null;
 	}
 
 }
